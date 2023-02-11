0734e763ea8fb4d4f51ab701dd39082c411f5d09
yamkant <dev.yamkim@gmail.com>
2023-02-12 07:27:59 +0900

    feat: #1 [Sequelize] sequelize 기본 설정 및 db 연동

--- sequelize_study/code/server.js
+++ sequelize_study/code/server.js
@@ -1,6 +1,30 @@
 'use strict';
 
 const express = require('express');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME
+  , process.env.DB_USER
+  , process.env.DB_PASSWORD
+  , {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }, {
+    logging: (...msg) => console.log(msg),
+  }
+)
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  });
+
+
 
 // Constants
 const PORT = process.env.PORT;
