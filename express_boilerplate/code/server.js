0636350999615a89b1b92b3f934fde397bac4f28
yamkant <dev.yamkim@gmail.com>
2023-02-11 20:47:24 +0900

    feat: express를 위한 dockerize 환경 설정

---
+++ express_boilerplate/code/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
